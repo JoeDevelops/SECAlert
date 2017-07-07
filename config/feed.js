7ca9a16da6021bccc561226d3b74af48e504e209
JoeDevelops <joe@joedevelops.com>
2017-07-07 10:55:03 -0700

    Added config files for rss URLS and parsing interval in minutes.

---
+++ config/feed.js
@@ -0,0 +1,7 @@
+var feed = {};
+
+feed.tenQURL = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=10-Q&company=&dateb=&owner=include&start=0&count=40&output=atom";
+feed.tenKURL = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=10-K&company=&dateb=&owner=include&start=0&count=40&output=atom";
+feed.parseInterval = .1;
+
+module.exports = feed;
